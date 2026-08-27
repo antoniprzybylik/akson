50aad62a5632daaae272c71d50dd24b9252e655d
Antoni Przybylik <antoni@taon.io>
2026-08-28 00:02:17 +0200

    Added missing imports and type annotations to _validation.py.

--- src/akson/_validation.py
+++ src/akson/_validation.py
@@ -1,4 +1,14 @@
-def validate_tensor_shape_with_names(tensor, tensor_name, sizes, size_names, size_descs):
+from typing import List, Optional
+import torch
+
+
+def validate_tensor_shape_with_names(
+        tensor: torch.Tensor,
+        tensor_name: str,
+        sizes: List[Optional[int]],
+        size_names: List[str],
+        size_descs: List[str]
+    ) -> None:
     ok = True
     if tensor.ndim != len(sizes):
         ok = False
@@ -19,11 +29,21 @@ def validate_tensor_shape_with_names(tensor, tensor_name, sizes, size_names, siz
         msg += f". Got shape {tensor.shape}"
         raise ValueError(msg)
 
-def validate_tensor(tensor, name, expected_shape):
+def validate_tensor(
+        tensor: torch.Tensor,
+        name: str,
+        expected_shape: List[int]
+    ) -> None:
     if tensor.shape != expected_shape:
         raise ValueError(f"Expected {name} to have shape {expected_shape}. Got {tensor.shape}")
 
-def validate_and_move_optional_tensor(tensor, tensor_name, expected_shape, desired_dtype, desired_device) -> Optional[torch.Tensor]:
+def validate_and_move_optional_tensor(
+        tensor: Optional[torch.Tensor],
+        tensor_name: str,
+        expected_shape: List[int],
+        desired_dtype: torch.dtype,
+        desired_device: torch.device
+    ) -> Optional[torch.Tensor]:
     if tensor is not None:
         if tensor.shape != expected_shape:
             raise ValueError(f"{tensor_name} has bad shape {tensor.shape}. Expected {expected_shape}")
@@ -31,6 +51,11 @@ def validate_and_move_optional_tensor(tensor, tensor_name, expected_shape, desir
     else:
         return None
 
-def validate_optional_tensors_le(tensor1, name1, tensor2, name2):
+def validate_optional_tensors_le(
+        tensor1: torch.Tensor,
+        name1: str,
+        tensor2: torch.Tensor,
+        name2: str
+    ) -> None:
     if tensor1 is not None and tensor2 is not None and (tensor1 > tensor2).any():
         raise ValueError(f"{name1} must be <= {name2} elementwise, but it is not")
